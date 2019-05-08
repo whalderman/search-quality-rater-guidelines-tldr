66311fa9bca72f99aa2a05cd9bb9651f5daba9b9
thewarrman <halderman.warren@gmail.com>
2019-05-08 14:46:13 +0900

    update file names

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -73,57 +73,57 @@ module.exports = {
 						title: "General Guidelines Overview",
 						children: [
 							"/general-guidelines-overview/",
-							"/general-guidelines-overview/introduction-to-search-quality-rating"
+							"/general-guidelines-overview/0-introduction-to-search-quality-rating"
 						]
 					},
 					{
 						title: "Page Quality Rating Guideline",
 						children: [
-							"/page-quality-rating-guideline/introduction-to-page-quality-rating",
-							"/page-quality-rating-guideline/understanding-webpages-and-websites",
-							"/page-quality-rating-guideline/overall-page-quality-rating",
-							"/page-quality-rating-guideline/high-quality-pages",
-							"/page-quality-rating-guideline/highest-quality-pages",
-							"/page-quality-rating-guideline/low-quality-pages",
-							"/page-quality-rating-guideline/lowest-quality-pages",
-							"/page-quality-rating-guideline/medium-quality-pages",
-							"/page-quality-rating-guideline/page-quality-rating-tasks",
-							"/page-quality-rating-guideline/page-quality-criteria-for-specific-types-of-pages",
-							"/page-quality-rating-guideline/page-quality-rating-faqs"
+							"/page-quality-rating-guideline/1-introduction-to-page-quality-rating",
+							"/page-quality-rating-guideline/2-understanding-webpages-and-websites",
+							"/page-quality-rating-guideline/3-overall-page-quality-rating",
+							"/page-quality-rating-guideline/4-high-quality-pages",
+							"/page-quality-rating-guideline/5-highest-quality-pages",
+							"/page-quality-rating-guideline/6-low-quality-pages",
+							"/page-quality-rating-guideline/7-lowest-quality-pages",
+							"/page-quality-rating-guideline/8-medium-quality-pages",
+							"/page-quality-rating-guideline/9-page-quality-rating-tasks",
+							"/page-quality-rating-guideline/10-page-quality-criteria-for-specific-types-of-pages",
+							"/page-quality-rating-guideline/11-page-quality-rating-faqs"
 						]
 					},
 					{
 						title: "Understanding Mobile User Needs",
 						children: [
-							"/understanding-mobile-user-needs/understanding-mobile-users-queries-and-results"
+							"/understanding-mobile-user-needs/12-understanding-mobile-users-queries-and-results"
 						]
 					},
 					{
 						title: "Needs Met Rating Guideline",
 						children: [
-							"/needs-met-rating-guideline/rating-using-the-needs-met-scale",
-							"/needs-met-rating-guideline/rating-porn-foreign-language-did-not-load-and-upsetting-offensive-results",
-							"/needs-met-rating-guideline/the-relationship-between-e-a-t-and-needs-met",
-							"/needs-met-rating-guideline/rating-queries-with-multiple-interpretations-and-intents",
-							"/needs-met-rating-guideline/specificity-of-queries-and-landing-pages",
-							"/needs-met-rating-guideline/needs-met-rating-and-freshness",
-							"/needs-met-rating-guideline/misspelled-and-mistyped-queries-and-results",
-							"/needs-met-rating-guideline/non-fully-meets-results-for-url-queries",
-							"/needs-met-rating-guideline/product-queries-importance-of-browsing-and-researching",
-							"/needs-met-rating-guideline/rating-visit-in-person-intent-queries",
-							"/needs-met-rating-guideline/rating-english-language-results-in-non-english-locales"
+							"/needs-met-rating-guideline/13-rating-using-the-needs-met-scale",
+							"/needs-met-rating-guideline/14-rating-porn-foreign-language-did-not-load-and-upsetting-offensive-results",
+							"/needs-met-rating-guideline/15-the-relationship-between-e-a-t-and-needs-met",
+							"/needs-met-rating-guideline/16-rating-queries-with-multiple-interpretations-and-intents",
+							"/needs-met-rating-guideline/17-specificity-of-queries-and-landing-pages",
+							"/needs-met-rating-guideline/18-needs-met-rating-and-freshness",
+							"/needs-met-rating-guideline/19-misspelled-and-mistyped-queries-and-results",
+							"/needs-met-rating-guideline/20-non-fully-meets-results-for-url-queries",
+							"/needs-met-rating-guideline/21-product-queries-importance-of-browsing-and-researching",
+							"/needs-met-rating-guideline/22-rating-visit-in-person-intent-queries",
+							"/needs-met-rating-guideline/23-rating-english-language-results-in-non-english-locales"
 						]
 					},
 					{
 						title: "Appendix: Using the Evaluation Platform",
 						children: [
-							"/appendix-using-the-evaluation-platform/overview",
-							"/appendix-using-the-evaluation-platform/acquiring-tasks",
-							"/appendix-using-the-evaluation-platform/rating-tasks-using-the-rating-interface",
-							"/appendix-using-the-evaluation-platform/releasing-tasks",
-							"/appendix-using-the-evaluation-platform/understanding-the-user-location-on-the-task-page",
-							"/appendix-using-the-evaluation-platform/reporting-duplicate-results-in-tasks",
-							"/appendix-using-the-evaluation-platform/simplified-needs-met-tasks"
+							"/appendix-using-the-evaluation-platform/24-overview",
+							"/appendix-using-the-evaluation-platform/25-acquiring-tasks",
+							"/appendix-using-the-evaluation-platform/26-rating-tasks-using-the-rating-interface",
+							"/appendix-using-the-evaluation-platform/27-releasing-tasks",
+							"/appendix-using-the-evaluation-platform/28-understanding-the-user-location-on-the-task-page",
+							"/appendix-using-the-evaluation-platform/29-reporting-duplicate-results-in-tasks",
+							"/appendix-using-the-evaluation-platform/30-simplified-needs-met-tasks"
 						]
 					}
 				],
