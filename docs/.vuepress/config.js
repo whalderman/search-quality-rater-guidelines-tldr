423e1c1bae9eb5966c2fa2135cbd3fb41a4e5358
thewarrman <halderman.warren@gmail.com>
2019-05-23 16:26:02 +0900

    reorganize files

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,68 +1,100 @@
 module.exports = {
 	themeConfig: {
-		width: "900px",
+		plugins: {
+			'container': {
+				type: 'ceo',
+				defaultTitle: 'CEO TL;DR',
+				before: info => `<div class='ceo'><p class='title'>${info}</p>`,
+				after: '</div>'
+			},
+			'container': {
+				type: 'sales',
+				defaultTitle: 'Sales TL;DR',
+				before: info => `<div class='sales'><p class='title'>${info}</p>`,
+				after: '</div>'
+			},
+			'container': {
+				type: 'dev',
+				defaultTitle: 'Developer TL;DR',
+				before: info => `<div class='dev'><p class='title'>${info}</p>`,
+				after: '</div>'
+			},
+			'container': {
+				type: 'design',
+				defaultTitle: 'Designer TL;DR',
+				before: info => `<div class='design'><p class='title'>${info}</p>`,
+				after: '</div>'
+			}
+		},
+		width: '900px',
+		lastUpdated: 'Updated',
+		repo: 'thewarrman/qrg-bci',
+		docsDir: 'docs',
+		editLinks: true,
+		editLinkText: 'Edit on GitHub',
+		evergreen: true,
 		locales: {
-			"/": {
-				lang: "en",
-				title: "Google's Quality Rater Guidelines",
-				description: "This is where you learn about Google's SEO advisory.",
+			'/': {
+				lang: 'en',
+				title: 'Google\'s Quality Rater Guidelines',
+				description: 'This is where you learn about Google\'s SEO advisory.',
 				nav: [
 					{
-						text: "🔰 Got Anything... Useful?", link: "/guidance/"
+						text: '🔰 Got Anything... Useful?', link: '/guidance/'
 					},
 					{
-						text: "Who Did This?", link: "/about/"
+						text: 'Who Did This?', link: '/about/'
 					},
 					{
-						text: "Ain't Nobody Got Time For This", items: [
+						text: 'Ain\'t Nobody Got Time For This', items: [
 							{
-								text: "Speak to me in...", items: [
-									{ text: "CEO Terms", link: "/seo-takeaways/ceos" },
-									{ text: "Sales Terms", link: "/seo-takeaways/sales" },
-									{ text: "Developer Terms", link: "/seo-takeaways/developers" },
-									{ text: "Designer Terms", link: "/seo-takeaways/designers" }
+								text: 'Speak to me in...', items: [
+									{ text: 'CEO Jargon', link: '/seo-takeaways/ceos' },
+									{ text: 'Sales Jargon', link: '/seo-takeaways/sales' },
+									{ text: 'Developer Jargon', link: '/seo-takeaways/developers' },
+									{ text: 'Designer Jargon', link: '/seo-takeaways/designers' }
 								]
 							}
 						]
 					},
 					{
-						text: "Digital Marketing Services", items: [
+						text: 'Digital Marketing Services', items: [
 							{
-								text: "U.S.", items: [
-									{ text: "English", link: "https://www.bruceclay.com/" }
+								text: 'U.S.', items: [
+									{ text: 'English', link: 'https://www.bruceclay.com/' }
 								]
 							},
 							{
-								text: "Europe", items: [
-									{ text: "English", link: "https://www.bruceclay.com/eu/" },
-									{ text: "Italian", link: "https://www.bruceclay.com/eu-it/" }
+								text: 'Europe', items: [
+									{ text: 'English', link: 'https://www.bruceclay.com/eu/' },
+									{ text: 'Italian', link: 'https://www.bruceclay.com/eu-it/' }
 								]
 							},
 							{
-								text: "Australia", items: [
-									{ text: "English", link: "https://www.bruceclay.com/au/" }
+								text: 'Australia', items: [
+									{ text: 'English', link: 'https://www.bruceclay.com/au/' }
 								]
 							},
 							{
-								text: "India", items: [
-									{ text: "English", link: "https://www.bruceclay.com/in/" }
+								text: 'India', items: [
+									{ text: 'English', link: 'https://www.bruceclay.com/in/' }
 								]
 							},
 							{
-								text: "Brazil", items: [
-									{ text: "English", link: "https://www.bruceclay.com/br/" },
-									{ text: "Portuguesa", link: "https://www.bruceclay.com/br-pt/" }
+								text: 'Brazil', items: [
+									{ text: 'English', link: 'https://www.bruceclay.com/br/' },
+									{ text: 'Portuguesa', link: 'https://www.bruceclay.com/br-pt/' }
 								]
 							},
 							{
-								text: "Japan", items: [
-									{ text: "English", link: "https://www.bruceclay.com/jp/" },
-									{ text: "日本語", link: "https://bruceclay.jpn.com/" }
+								text: 'Japan', items: [
+									{ text: 'English', link: 'https://www.bruceclay.com/jp/' },
+									{ text: '日本語', link: 'https://bruceclay.jpn.com/' }
 								]
 							},
 							{
-								text: "Middle East", items: [
-									{ text: "English", link: "https://www.bruceclay.com/me/" }
+								text: 'Middle East', items: [
+									{ text: 'English', link: 'https://www.bruceclay.com/me/' }
 								]
 							}
 						]
@@ -71,90 +103,64 @@ module.exports = {
 				sidebarDepth: 1,
 				sidebar: [
 					{
-						title: "General Guidelines Overview",
+						title: 'General Guidelines Overview',
 						children: [
-							"/general-guidelines-overview/",
-							"/general-guidelines-overview/0-introduction-to-search-quality-rating"
+							'/qrg/general-guidelines-overview/',
+							'/qrg/general-guidelines-overview/0-introduction-to-search-quality-rating'
 						]
 					},
 					{
-						title: "Page Quality Rating Guideline",
+						title: 'Page Quality Rating Guideline',
 						children: [
-							"/page-quality-rating-guideline/1-introduction-to-page-quality-rating",
-							"/page-quality-rating-guideline/2-understanding-webpages-and-websites",
-							"/page-quality-rating-guideline/3-overall-page-quality-rating",
-							"/page-quality-rating-guideline/4-high-quality-pages",
-							"/page-quality-rating-guideline/5-highest-quality-pages",
-							"/page-quality-rating-guideline/6-low-quality-pages",
-							"/page-quality-rating-guideline/7-lowest-quality-pages",
-							"/page-quality-rating-guideline/8-medium-quality-pages",
-							"/page-quality-rating-guideline/9-page-quality-rating-tasks",
-							"/page-quality-rating-guideline/10-page-quality-criteria-for-specific-types-of-pages",
-							"/page-quality-rating-guideline/11-page-quality-rating-faqs"
+							'/qrg/page-quality-rating-guideline/1-introduction-to-page-quality-rating',
+							'/qrg/page-quality-rating-guideline/2-understanding-webpages-and-websites',
+							'/qrg/page-quality-rating-guideline/3-overall-page-quality-rating',
+							'/qrg/page-quality-rating-guideline/4-high-quality-pages',
+							'/qrg/page-quality-rating-guideline/5-highest-quality-pages',
+							'/qrg/page-quality-rating-guideline/6-low-quality-pages',
+							'/qrg/page-quality-rating-guideline/7-lowest-quality-pages',
+							'/qrg/page-quality-rating-guideline/8-medium-quality-pages',
+							'/qrg/page-quality-rating-guideline/9-page-quality-rating-tasks',
+							'/qrg/page-quality-rating-guideline/10-page-quality-criteria-for-specific-types-of-pages',
+							'/qrg/page-quality-rating-guideline/11-page-quality-rating-faqs'
 						]
 					},
 					{
-						title: "Understanding Mobile User Needs",
+						title: 'Understanding Mobile User Needs',
 						children: [
-							"/understanding-mobile-user-needs/12-understanding-mobile-users-queries-and-results"
+							'/qrg/understanding-mobile-user-needs/12-understanding-mobile-users-queries-and-results'
 						]
 					},
 					{
-						title: "Needs Met Rating Guideline",
+						title: 'Needs Met Rating Guideline',
 						children: [
-							"/needs-met-rating-guideline/13-rating-using-the-needs-met-scale",
-							"/needs-met-rating-guideline/14-rating-porn-foreign-language-did-not-load-and-upsetting-offensive-results",
-							"/needs-met-rating-guideline/15-the-relationship-between-e-a-t-and-needs-met",
-							"/needs-met-rating-guideline/16-rating-queries-with-multiple-interpretations-and-intents",
-							"/needs-met-rating-guideline/17-specificity-of-queries-and-landing-pages",
-							"/needs-met-rating-guideline/18-needs-met-rating-and-freshness",
-							"/needs-met-rating-guideline/19-misspelled-and-mistyped-queries-and-results",
-							"/needs-met-rating-guideline/20-non-fully-meets-results-for-url-queries",
-							"/needs-met-rating-guideline/21-product-queries-importance-of-browsing-and-researching",
-							"/needs-met-rating-guideline/22-rating-visit-in-person-intent-queries",
-							"/needs-met-rating-guideline/23-rating-english-language-results-in-non-english-locales"
+							'/qrg/needs-met-rating-guideline/13-rating-using-the-needs-met-scale',
+							'/qrg/needs-met-rating-guideline/14-rating-porn-foreign-language-did-not-load-and-upsetting-offensive-results',
+							'/qrg/needs-met-rating-guideline/15-the-relationship-between-e-a-t-and-needs-met',
+							'/qrg/needs-met-rating-guideline/16-rating-queries-with-multiple-interpretations-and-intents',
+							'/qrg/needs-met-rating-guideline/17-specificity-of-queries-and-landing-pages',
+							'/qrg/needs-met-rating-guideline/18-needs-met-rating-and-freshness',
+							'/qrg/needs-met-rating-guideline/19-misspelled-and-mistyped-queries-and-results',
+							'/qrg/needs-met-rating-guideline/20-non-fully-meets-results-for-url-queries',
+							'/qrg/needs-met-rating-guideline/21-product-queries-importance-of-browsing-and-researching',
+							'/qrg/needs-met-rating-guideline/22-rating-visit-in-person-intent-queries',
+							'/qrg/needs-met-rating-guideline/23-rating-english-language-results-in-non-english-locales'
 						]
 					},
 					{
-						title: "Appendix: Using the Evaluation Platform",
+						title: 'Appendix: Using the Evaluation Platform',
 						children: [
-							"/appendix-using-the-evaluation-platform/24-overview",
-							"/appendix-using-the-evaluation-platform/25-acquiring-tasks",
-							"/appendix-using-the-evaluation-platform/26-rating-tasks-using-the-rating-interface",
-							"/appendix-using-the-evaluation-platform/27-releasing-tasks",
-							"/appendix-using-the-evaluation-platform/28-understanding-the-user-location-on-the-task-page",
-							"/appendix-using-the-evaluation-platform/29-reporting-duplicate-results-in-tasks",
-							"/appendix-using-the-evaluation-platform/30-simplified-needs-met-tasks"
+							'/qrg/appendix-using-the-evaluation-platform/24-overview',
+							'/qrg/appendix-using-the-evaluation-platform/25-acquiring-tasks',
+							'/qrg/appendix-using-the-evaluation-platform/26-rating-tasks-using-the-rating-interface',
+							'/qrg/appendix-using-the-evaluation-platform/27-releasing-tasks',
+							'/qrg/appendix-using-the-evaluation-platform/28-understanding-the-user-location-on-the-task-page',
+							'/qrg/appendix-using-the-evaluation-platform/29-reporting-duplicate-results-in-tasks',
+							'/qrg/appendix-using-the-evaluation-platform/30-simplified-needs-met-tasks'
 						]
 					}
-				],
-				lastUpdated: "Updated",
-				repo: "thewarrman/qrg-bci",
-				docsDir: "docs",
-				editLinks: true,
-				editLinkText: "Edit on GitHub",
-				evergreen: true
+				]
 			}
 		}
-	},
-	plugins: [
-		["container", {
-			type: "ceo",
-			defaultTitle: "CEO TL;DR",
-			before: info => `<div class="ceo"><p class="title">${info}</p>`,
-			after: "</div>"
-		}],
-		["container", {
-			type: "sales",
-			defaultTitle: "Sales TL;DR"
-		}],
-		["container", {
-			type: "dev",
-			defaultTitle: "Developer TL;DR"
-		}],
-		["container", {
-			type: "des",
-			defaultTitle: "Designer TL;DR"
-		}],
-	]
+	}
 }
--- docs/guidance/index.md
+++ docs/guidance/index.md
@@ -1,38 +1,38 @@
-# 🔰 Got Anything... Useful?
+# 🔰 Got Anything... Useful？
 
 [We certainly _hope_ it's useful](#choose-your-specialty).
 
-## The Point of this Site.
+## The Point of this Site
 
-## Why Does Google Make This Difficult?
-
-
+We're trying to save you time.
 
 Google provides a lot of recommendations and guidance, albeit through riddles and loose, vague language. However, they **have** to do this. By keeping things vague, they achieve the following:
 
 1. Bad actors (black-hat SEOs) will have a _slighty_ more difficult time figuring out what they can do to exploit Google's algorithm.
-2.
+2. Wow
 
 ## Choose your Specialty
 
-### I specialize in SEO.
+### I specialize in SEO
 
-**That's** weird... sorry to say, you're gonna have to peruse this entire thing. **_Or_** you can just pay attention to the [TL;DR]()s.
+**That's** weird... sorry to say, you're gonna have to peruse this entire thing. **_Or_** you can just browse through the [TL;DR](/tldr)s.
 
-Additionally, for those of you interested in *black hat* SEO, all you need can be found [here](https://bit.ly/2U6ZdlL).
+Also, for those of you interested in *black hat* SEO, all you need can be found [here](https://bit.ly/2U6ZdlL).
 
-### Local Business.
+### Local Business
 
-We got ya covered, yer gonna want to take a gander at [these here sections]().
+We got ya covered, you're going to want to take a gander at [these here sections](/tldr/local).
 
-### Online Business.
+### Online Business
 
 Need some *scalable* solutions? *Scalable* ROIs? Or *data-driven deliverables* that happen to be **_scales_**? Well **hoo boy** do I-!
 
-> Just give me the goods.
+::: danger
+Just give me the goods.
+:::
 
-<small><small>[Right this way.]()</small></small>
+[Right this way.](/tldr/online)
 
 ::: ceo
-We've attempted to keep things as short and concise as possible for the CEOs out there. We try to keep these TL;DRs in layman's terms.
+We've attempted to keep things as short and concise as possible for the CEOs out there. These get straight to the bottom line.
 :::
