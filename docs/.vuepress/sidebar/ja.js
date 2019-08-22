b6813f0453ffa9db77defe8f76fc72cad53d05c8
thewarrman <halderman.warren@gmail.com>
2019-08-22 18:08:45 +0900

    less tabs more spaces

--- docs/.vuepress/sidebar/ja.js
+++ docs/.vuepress/sidebar/ja.js
@@ -1,71 +1,71 @@
 module.exports = {
-	'/ja/tldr/': [
-		{
-			title: 'TL;DR',
-			children: [
-				'/ja/tldr/for-everyone',
-				'/ja/tldr/for-developers',
-				'/ja/tldr/for-designers',
-				'/ja/tldr/quality-characteristics'
-			]
-		}],
-	'/ja/': [
-		{
-			title: '検索品質評価ガイドラインの概要',
-			children: [
-				'/ja/qrg/general-guidelines-overview/',
-				'/ja/qrg/general-guidelines-overview/0-introduction-to-search-quality-rating'
-			]
-		},
-		{
-			title: 'Page Quality評価のガイドライン',
-			children: [
-				'/ja/qrg/page-quality-rating-guideline/1-introduction-to-page-quality-rating',
-				'/ja/qrg/page-quality-rating-guideline/2-understanding-webpages-and-websites',
-				'/ja/qrg/page-quality-rating-guideline/3-overall-page-quality-rating',
-				'/ja/qrg/page-quality-rating-guideline/4-high-quality-pages',
-				'/ja/qrg/page-quality-rating-guideline/5-highest-quality-pages',
-				'/ja/qrg/page-quality-rating-guideline/6-low-quality-pages',
-				'/ja/qrg/page-quality-rating-guideline/7-lowest-quality-pages',
-				'/ja/qrg/page-quality-rating-guideline/8-medium-quality-pages',
-				'/ja/qrg/page-quality-rating-guideline/9-page-quality-rating-tasks',
-				'/ja/qrg/page-quality-rating-guideline/10-page-quality-criteria-for-specific-types-of-pages',
-				'/ja/qrg/page-quality-rating-guideline/11-page-quality-rating-faqs'
-			]
-		},
-		{
-			title: 'モバイルユーザーのニーズ把握',
-			children: [
-				'/ja/qrg/understanding-mobile-user-needs/12-understanding-mobile-users-queries-and-results'
-			]
-		},
-		{
-			title: 'Needs Met評価のガイドライン',
-			children: [
-				'/ja/qrg/needs-met-rating-guideline/13-rating-using-the-needs-met-scale',
-				'/ja/qrg/needs-met-rating-guideline/14-rating-porn-foreign-language-did-not-load-and-upsetting-offensive-results',
-				'/ja/qrg/needs-met-rating-guideline/15-the-relationship-between-page-quality-and-needs-met',
-				'/ja/qrg/needs-met-rating-guideline/16-rating-queries-with-multiple-interpretations-and-intents',
-				'/ja/qrg/needs-met-rating-guideline/17-specificity-of-queries-and-landing-pages',
-				'/ja/qrg/needs-met-rating-guideline/18-needs-met-rating-and-freshness',
-				'/ja/qrg/needs-met-rating-guideline/19-misspelled-and-mistyped-queries-and-results',
-				'/ja/qrg/needs-met-rating-guideline/20-non-fully-meets-results-for-url-queries',
-				'/ja/qrg/needs-met-rating-guideline/21-product-queries-importance-of-browsing-and-researching',
-				'/ja/qrg/needs-met-rating-guideline/22-rating-visit-in-person-intent-queries',
-				'/ja/qrg/needs-met-rating-guideline/23-rating-english-language-results-in-non-english-locales'
-			]
-		},
-		{
-			title: '付録：Evaluation Platformを使う',
-			children: [
-				'/ja/qrg/appendix-using-the-evaluation-platform/24-overview',
-				'/ja/qrg/appendix-using-the-evaluation-platform/25-acquiring-tasks',
-				'/ja/qrg/appendix-using-the-evaluation-platform/26-rating-tasks-using-the-rating-interface',
-				'/ja/qrg/appendix-using-the-evaluation-platform/27-releasing-tasks',
-				'/ja/qrg/appendix-using-the-evaluation-platform/28-understanding-the-user-location-on-the-task-page',
-				'/ja/qrg/appendix-using-the-evaluation-platform/29-reporting-duplicate-results-in-tasks',
-				'/ja/qrg/appendix-using-the-evaluation-platform/30-simplified-needs-met-tasks'
-			]
-		}
-	]
+  '/ja/tldr/': [
+    {
+      title: 'TL;DR',
+      children: [
+        '/ja/tldr/for-everyone',
+        '/ja/tldr/for-developers',
+        '/ja/tldr/for-designers',
+        '/ja/tldr/quality-characteristics'
+      ]
+    }],
+  '/ja/': [
+    {
+      title: '検索品質評価ガイドラインの概要',
+      children: [
+        '/ja/qrg/general-guidelines-overview/',
+        '/ja/qrg/general-guidelines-overview/0-introduction-to-search-quality-rating'
+      ]
+    },
+    {
+      title: 'Page Quality評価のガイドライン',
+      children: [
+        '/ja/qrg/page-quality-rating-guideline/1-introduction-to-page-quality-rating',
+        '/ja/qrg/page-quality-rating-guideline/2-understanding-webpages-and-websites',
+        '/ja/qrg/page-quality-rating-guideline/3-overall-page-quality-rating',
+        '/ja/qrg/page-quality-rating-guideline/4-high-quality-pages',
+        '/ja/qrg/page-quality-rating-guideline/5-highest-quality-pages',
+        '/ja/qrg/page-quality-rating-guideline/6-low-quality-pages',
+        '/ja/qrg/page-quality-rating-guideline/7-lowest-quality-pages',
+        '/ja/qrg/page-quality-rating-guideline/8-medium-quality-pages',
+        '/ja/qrg/page-quality-rating-guideline/9-page-quality-rating-tasks',
+        '/ja/qrg/page-quality-rating-guideline/10-page-quality-criteria-for-specific-types-of-pages',
+        '/ja/qrg/page-quality-rating-guideline/11-page-quality-rating-faqs'
+      ]
+    },
+    {
+      title: 'モバイルユーザーのニーズ把握',
+      children: [
+        '/ja/qrg/understanding-mobile-user-needs/12-understanding-mobile-users-queries-and-results'
+      ]
+    },
+    {
+      title: 'Needs Met評価のガイドライン',
+      children: [
+        '/ja/qrg/needs-met-rating-guideline/13-rating-using-the-needs-met-scale',
+        '/ja/qrg/needs-met-rating-guideline/14-rating-porn-foreign-language-did-not-load-and-upsetting-offensive-results',
+        '/ja/qrg/needs-met-rating-guideline/15-the-relationship-between-page-quality-and-needs-met',
+        '/ja/qrg/needs-met-rating-guideline/16-rating-queries-with-multiple-interpretations-and-intents',
+        '/ja/qrg/needs-met-rating-guideline/17-specificity-of-queries-and-landing-pages',
+        '/ja/qrg/needs-met-rating-guideline/18-needs-met-rating-and-freshness',
+        '/ja/qrg/needs-met-rating-guideline/19-misspelled-and-mistyped-queries-and-results',
+        '/ja/qrg/needs-met-rating-guideline/20-non-fully-meets-results-for-url-queries',
+        '/ja/qrg/needs-met-rating-guideline/21-product-queries-importance-of-browsing-and-researching',
+        '/ja/qrg/needs-met-rating-guideline/22-rating-visit-in-person-intent-queries',
+        '/ja/qrg/needs-met-rating-guideline/23-rating-english-language-results-in-non-english-locales'
+      ]
+    },
+    {
+      title: '付録：Evaluation Platformを使う',
+      children: [
+        '/ja/qrg/appendix-using-the-evaluation-platform/24-overview',
+        '/ja/qrg/appendix-using-the-evaluation-platform/25-acquiring-tasks',
+        '/ja/qrg/appendix-using-the-evaluation-platform/26-rating-tasks-using-the-rating-interface',
+        '/ja/qrg/appendix-using-the-evaluation-platform/27-releasing-tasks',
+        '/ja/qrg/appendix-using-the-evaluation-platform/28-understanding-the-user-location-on-the-task-page',
+        '/ja/qrg/appendix-using-the-evaluation-platform/29-reporting-duplicate-results-in-tasks',
+        '/ja/qrg/appendix-using-the-evaluation-platform/30-simplified-needs-met-tasks'
+      ]
+    }
+  ]
 };
