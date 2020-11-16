bd298d7c378d46b60fc57f65b034ca2507c7ebc1
Warren Halderman <halderman.warren@gmail.com>
2020-11-16 09:53:39 +0900

    remove irrelevant links

--- docs/.vuepress/nav/en.js
+++ docs/.vuepress/nav/en.js
@@ -8,42 +8,6 @@ module.exports = [
   // {
   //   text: 'About', link: '/about/'
   // },
-  {
-    text: 'Need Marketing Help?', items: [
-      {
-        text: 'Japan', items: [
-          { text: 'English', link: 'https://www.bruceclay.com/jp/' },
-          { text: 'Japanese', link: 'https://bruceclay.jpn.com/' }
-        ]
-      },
-      {
-        text: 'U.S.', items: [
-          { text: 'English', link: 'https://www.bruceclay.com/' }
-        ]
-      },
-      {
-        text: 'Europe', items: [
-          { text: 'English', link: 'https://www.bruceclay.com/eu/' }
-        ]
-      },
-      {
-        text: 'Australia', items: [
-          { text: 'English', link: 'https://www.bruceclay.com/au/' }
-        ]
-      },
-      {
-        text: 'India', items: [
-          { text: 'English', link: 'https://www.bruceclay.com/in/' }
-        ]
-      },
-      {
-        text: 'Middle East', items: [
-          { text: 'English', link: 'https://www.bruceclay.com/me/' }
-        ]
-      }
-    ]
-  }
-  // ,
   // {
   //   text: 'Got a Question?', link: 'https://docs.google.com/forms/d/e/1FAIpQLSePiGKAUnHPhNvBQVq8V7CUB83q9H0ahO5X_QsmmolRFfcqMg/viewform?usp=sf_link'
   // }
--- docs/.vuepress/nav/ja.js
+++ docs/.vuepress/nav/ja.js
@@ -8,42 +8,6 @@ module.exports = [
   // {
   //   text: 'この情報について', link: '/ja/about/'
   // },
-  {
-    text: 'Webマーケティング サービス', items: [
-      {
-        text: '日本', items: [
-          { text: '英語', link: 'https://www.bruceclay.com/jp/' },
-          { text: '日本語', link: 'https://bruceclay.jpn.com/' }
-        ]
-      },
-      {
-        text: '米国', items: [
-          { text: '英語', link: 'https://www.bruceclay.com/' }
-        ]
-      },
-      {
-        text: '欧州', items: [
-          { text: '英語', link: 'https://www.bruceclay.com/eu/' }
-        ]
-      },
-      {
-        text: 'オーストラリア', items: [
-          { text: '英語', link: 'https://www.bruceclay.com/au/' }
-        ]
-      },
-      {
-        text: 'インド', items: [
-          { text: '英語', link: 'https://www.bruceclay.com/in/' }
-        ]
-      },
-      {
-        text: '中東', items: [
-          { text: '英語', link: 'https://www.bruceclay.com/me/' }
-        ]
-      }
-    ]
-  }
-  // ,
   // {
   //   text: '質問？', link: 'https://docs.google.com/forms/d/e/1FAIpQLSftex23Tnrj_Jox8_8j30k7WZAsvCbkawBs8MEvdj8K5ZF5qQ/viewform?usp=sf_link'
   // }
